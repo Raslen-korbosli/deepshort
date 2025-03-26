4f102a07d64bb23c2eb37c86b72549984c7d2774
Raslen Korbosli <raslen.korbosli01@gmail.com>
2025-03-26 15:25:06 +0100

    complete landing page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 
 export function Footer() {
   return (
-    <footer className="py-8 bg-card border-t-1 shadow-sm">
+    <footer className="py-8 bg-card border-t-1 ">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between  space-y-6 md:space-y-0 ">
         <div className="flex items-center space-x-4">
-          <Link href="/" className="text-2xl font-bold">
+          <Link href="#home" className="text-2xl font-bold">
             DeepShort
           </Link>
         </div>
@@ -16,23 +16,23 @@ export function Footer() {
             href="https://instagram.com/deepshort"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-gray-300 transition-colors"
+            className="hover:text-muted-foreground transition-colors"
           >
             <Instagram size={20} />
           </a>
           <a
-            href="https://github.com/deepshort"
+            href="https://github.com/Raslen-Korbosli"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-gray-300 transition-colors"
+            className="hover:text-muted-foreground transition-colors"
           >
             <Github size={20} />
           </a>
         </div>
       </div>
 
-      <div className="text-center text-gray-500 mt-6 text-sm">
-        © 2024 DeepShort. All rights reserved.
+      <div className="text-center text-muted-foreground mt-6 text-sm">
+        © 2024 DeepShort. All rights reserved.Raslen korbosli
       </div>
     </footer>
   );
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,17 @@
 import { ModeToggleButton } from '@/components/ModeToggleButton';
 import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import { Separator } from '@/components/ui/separator';
 import Image from 'next/image';
 import Link from 'next/link';
+import { LoginButtons } from './LoginButtons';
 export default function NavBar() {
   return (
     <nav className="sticky top-0 z-50  backdrop-blur-md shadow-sm">
@@ -21,9 +30,41 @@ export default function NavBar() {
           </div>
         </Link>
         <div className="space-x-4 flex justify-center items-center">
-          <Button variant="outline">Login</Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <div className="flex gap-2">
+                <Button variant="outline" className="cursor-pointer">
+                  Log In
+                </Button>
+                <Button className="hidden sm:block cursor-pointer">
+                  Sign Up
+                </Button>
+              </div>
+            </DialogTrigger>
 
-          <Button className="hidden sm:block">Sign Up</Button>
+            <DialogContent className="sm:grid sm:grid-cols-2 sm:grid-rows-3 lg:h-[550px] lg:w-[800px] sm:h-[450px] sm:w-[600px]  h-[450px] w-[300px] p-0 overflow-hidden  border-0 bg-secondary ">
+              <div>
+                <DialogHeader className="row-span-2 col-start-1 row-start-1 p-8 flex-1 text-center">
+                  <DialogTitle className=" text-xl sm:text-3xl font-semibold">
+                    Log in or sign up in seconds
+                  </DialogTitle>
+                  <DialogDescription className="">
+                    Use your email or another service to continue with deepshort
+                    (it’s free)!
+                  </DialogDescription>
+                </DialogHeader>
+                <LoginButtons />
+              </div>
+              <Image
+                src="/loginImage.png"
+                alt="login image"
+                className="row-span-3 col-start-2 row-start-1 scale-[1.01] sm:block hidden  "
+                layout="fill"
+                objectFit="cover"
+              />
+            </DialogContent>
+          </Dialog>
+
           <Separator
             className="SeparatorRoot"
             orientation="vertical"
